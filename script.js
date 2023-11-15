1270da8f1513e626301f7de8fc8625c1933661ad
Alex <sakalinski95@gmail.com>
2023-11-15 15:51:23 +0300

    Add function to get cookie value

--- script.js
+++ script.js
@@ -10,3 +10,15 @@ window.addEventListener('DOMContentLoaded', ()=>{
 accept_btn.addEventListener('click', ()=>{     
     document.cookie = "accept_cookies=true; max-age=86400; path=/";;
 });
+
+
+function getCookie(name) {
+    let nameEQ = name + "=";
+    let ca = document.cookie.split(';');
+    for(var i=0; i < ca.length; i++) {
+        let c = ca[i];
+        while (c.charAt(0)==' ') c = c.substring(1,c.length);
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+    }
+    return null;
+}
